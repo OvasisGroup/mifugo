a32dd7cc57cd0cde28c8dc1780bf58ac5edb5728
thekimpeople <thekimpeople@gmail.com>
2025-05-19 09:40:04 +0300

    first commit

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './login.component.html',
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log('Login:', this.loginForm.value);
+    }
+  }
+
+  // login.component.ts
+  showPassword: boolean = false;
+
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword;
+  }
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    baseUrl: 'http://3.136.142.78:7778/api/v1'
+  };
+  
